d6557bf4cc114cef82916a0b30aaedcbd66e523f
wendig <vatamany.l@gmail.com>
2018-11-13 21:07:32 +0100

    init

---
+++ src/components/Dashboard/Layout/ContentFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <nav>
+        <ul class="footer-menu">
+          <li>
+            <router-link :to="{path:'/admin'}">Dashboard</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright text-center">
+        Energy Station
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+  export default {}
+
+</script>
+<style>
+
+</style>
